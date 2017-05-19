2c6ccb99de90014c2e939bb6d595c45c7ac75b6f
Amy Volkert <Amy Volkert>
2017-05-18 21:07:05 -0600

    added getElementById

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,48 @@
-var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"
-];
-		var wins = 0;
-		var losses = 0;
-		var guessesLeft = 9;
-		var guessesSoFar = 0;
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = 0;
 
-		document.onkeyup = function() { var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	//User chooses a-z 
+	document.onkeyup = function() { 
 
-			console.log(userGuess);
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-			var computerGuess = options[Math.floor(Math.random()*options.length)];
+		console.log(userGuess);
 
-			console.log(computerGuess);
+		//Computer chooses a-z (at random)
+		var computerGuess = options[Math.floor(Math.random() * options.length)];
 
-			if (userGuess=='a' || userGuess=='b' || userGuess=='c' || userGuess=='d' || userGuess=='e') {
+		console.log(computerGuess);
+
+			//Capture the choices
+			//If userGuess equals computerGuess
+			if (userGuess=='a' || userGuess=='b' || userGuess=='c' || userGuess=='d' || userGuess=='e' || 
+				userGuess=='f' || userGuess=='g' || userGuess=='h' || userGuess=='i' || userGuess=='j' || 
+				userGuess=='k' || userGuess=='l' || userGuess=='m' || userGuess=='n' || userGuess=='o' || 
+				userGuess=='p' || userGuess=='q' || userGuess=='r' || userGuess=='s' || userGuess=='t' || 
+				userGuess=='u' || userGuess=='v' || userGuess=='w' || userGuess=='x' || userGuess=='y' || 
+				userGuess=='z') {
 				
 				if ((userGuess==computerGuess)) {
-				wins++;
-				alert("Wins: " + wins);
+				wins++; 
+				document.getElementById("demo").innerHTML = ("Wins: " + wins)
 				}
-			
+				//DOCUMENT WRITE??
+				//If userGuess does not equal computerGuess
 				if ((userGuess!=computerGuess)) {
 				guessesLeft--;
 				alert("Number of guesses left: " + guessesLeft);
 				}
-			}
-			
-			else {
+						
+				else {
 				alert("Please choose a letter.");
+				}
 			}
-	}
 
-	var html = "<p>Wins: " + wins + "</p>" +"<p>Losses: " + losses + "</p>" + "<p>Guesses left: " + guessesLeft +"</p" + "<p>Guesses So Far: " + guessesSoFar + "</p>"
+	}
+document.getElementById("demo").innerHTML = "Wins: " + wins;
+	// var html = "<p>Wins: " + wins + "</p>" +"<p>Losses: " + losses + "</p>" + "<p>Guesses left: " + guessesLeft +"</p" + "<p>Guesses So Far: " + guessesSoFar + "</p>";
 
