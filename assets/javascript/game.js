5c973e80435dd640e4cacc4f6419501606920bf6
Amy Volkert <Amy Volkert>
2017-05-20 18:40:28 -0600

    tested with minimum options

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 var wins = 0;
 var guessesSoFar = 0;
-var guessesLeft = 9;
+var guessesToStart = 9;
 var losses = 0;
 	
 	/*//Determine a win or loss
@@ -21,42 +21,28 @@ var losses = 0;
 	console.log(userGuess);
 
 	//Computer chooses a-z (at random)
-	var options = ["a", "b", "c"];
-// , "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
-// 	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"
+	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"];
+
 	var computerGuess = options[Math.floor(Math.random() * options.length)];
 
 	console.log(computerGuess);
 
-			//Compare the choices: if userGuess does not equal computerGuess
+		for (i = 0; i < 10; i++) {
+			//Compare the choices: if userGuess does not equal computerGuess 
 			if ((keyPress!=computerGuess)) {
+				var guessesLeft = guessesToStart - userGuess.length;
+
 				document.getElementById("displayGuessesLeft").innerHTML = "Numbers of Guesses Remaining: " + guessesLeft;
-				}		
+				document.getElementById("displayGuessesSoFar").innerHTML = "Guesses So Far: " + userGuess;
+
+			
+			}		
 
 			//Compare the choices: if userGuess equals computerGuess
 			if ((keyPress==computerGuess)) {
 				wins++; 
 				document.getElementById("displayWins").innerHTML = "Wins: " + wins;				
-				}
-			
-			
-			else {
-				document.getElementById("displayGuessesSoFar").innerHTML = ("Guesses So Far: " + userGuess)
-				}
-
-			
-			// 	
-			// 	
-
-						
-				
-				
-						
 			}
-
-	// }
-
-	// var html = "<p>Wins: " + wins + "</p>" +"<p>Losses: " + losses + "</p>" + "<p>Guesses left: " + guessesLeft +"</p" + "<p>Guesses So Far: " + guessesSoFar + "</p>";
-
-// else {
-// 				alert("Please choose a letter.");
+		}
+	}
